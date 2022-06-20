06b086dbce99df0e114ad0da0466120a371a0b98
rajwindersidhu92 <rajwin.kaur5@gmail.com>
2022-06-20 05:07:42 -0400

    sec 7

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestErrorsComponent
   ],
   imports: [
     BrowserModule,
