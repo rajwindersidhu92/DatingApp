2bbbd6995a477d91d8a664e06387c727d937d56d
rajwindersidhu92 <rajwin.kaur5@gmail.com>
2022-06-24 10:27:39 -0400

    addind editprofile page

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  member : Member;
+  user: User;
+  editForm: any;
+
+  constructor(private accountService : AccountService , 
+    private memberService : MembersService , private toastr : ToastrService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+   }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    this.memberService.getMember(this.user.username).subscribe(member => {
+      this.member = member;
+    })
+  }
+
+  updateMember(){
+    console.log(this.member);
+    this.toastr.success('Profile Updated Successfully');
+    this.editForm.reset(this.member);
+  }
+
+}
